89297fa2c97063472a77cf07fe323d8f486c6017
shubhamAyodhyavasi <shubhamAyodhyavasi@users.noreply.github.com>
2020-05-09 16:55:34 +0530

    Updates

---
+++ precache-manifest.78d27819bd1a7e80117a1908d804750d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aecbbb985aedae112760cce0fb1f2b24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a12cd0397e28a19f073a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f07683ff76477b2007eb",
+    "url": "/static/js/2.6345265f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6345265f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a12cd0397e28a19f073a",
+    "url": "/static/js/main.d835e9f5.chunk.js"
+  },
+  {
+    "revision": "751c80ddc5823a2a6930",
+    "url": "/static/js/runtime-main.831b94d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
